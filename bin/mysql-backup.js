c5512b579308ef39543f2b3bc7bf9ca8462750b6
Sasha Mile Rudan <mprinc@gmail.com>
2015-01-16 16:30:41 +0100

    Added reporting progress Added reporting errors Added printing stack of error Extended functionality to log only particular list of databases instead all Extended functionality to log only particular list of tables instead all in a database

---
+++ bin/mysql-backup.js
@@ -0,0 +1,64 @@
+#! /usr/bin/env /usr/local/bin/node
+'use strict';
+
+/* jshint node: true */
+
+process.on('uncaughtException', fail);
+
+var start   = +new Date();
+var which   = process.argv[process.argv.length - 1];
+var async   = require('async');
+var moment  = require('moment');
+var config  = require('../config');
+var dropbox = require('../lib/dropbox-sync');
+var mysql   = require('../lib/mysql-backup');
+var tarball = require('../lib/tarball');
+var clean   = require('../lib/clean');
+var outFile = config.mysql[which].file_prefix + 'mysql-' + moment().format('YYYYMMDD-HHmmss');
+var databases = config.mysql[which].databases;
+
+async.series({
+	connect: function (cb) {
+		console.log("[bin/mysql-backup.js:showDatabases] connecting to dropbox");
+		var connection = dropbox.connect({
+			key: config.drivers.dropbox.key,
+			secret: config.drivers.dropbox.secret
+		});
+		if(!connection) return null;
+
+		console.log("[bin/mysql-backup.js:showDatabases] setting dropbox folder");
+	    dropbox.setDirectory(config.mysql[which].directory);
+	
+		console.log("[bin/mysql-backup.js:showDatabases] connecting to DB server");
+	    connection = mysql.connect({
+	      host: config.mysql[which].hostname,
+	      user: config.mysql[which].username,
+	      password: config.mysql[which].password
+	    });
+	    if(!connection) return null;
+	
+	    cb();
+	  },
+  backup: mysql.backup.bind(mysql, outFile, databases),
+  archive: tarball.create.bind(tarball, outFile),
+  sync: dropbox.send.bind(dropbox, outFile),
+  clean: clean.bind(clean, outFile),
+  cleanOld: dropbox.cleanOld.bind(dropbox, config.mysql[which].rotate)
+}, function (err, args) {
+  if (err) return fail(err);
+
+  console.log('The MySQL backup has been completed successfully.\n');
+  console.log('Backed up %d table(s) from %d database(s).\n', args.backup.tables, args.backup.databases);
+  console.log('Total execution time: %d seconds', ((+new Date() - start) / 1000).toFixed(2));
+  console.log('\nThe backup file has been saved to your Dropbox: %s (%s)', args.sync.path, args.sync.humanSize);
+  process.exit(0);
+});
+
+function fail (err) {
+  //clean && clean(outFile);
+
+  console.error('The MySQL backup job has encountered a fatal error and could not continue.');
+  console.error("\tError: %s", JSON.stringify(err));
+  if(typeof err.stack !== 'undefined') console.error(err.stack);
+  process.exit(1);
+}
